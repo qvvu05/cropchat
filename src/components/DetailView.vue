1c52a8e182d8adc884edddf701516055f5e7bcfe
Quyen <qvvu05@gmail.com>
2018-05-17 18:48:33 +0700

    add before leave

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -3,7 +3,19 @@
   </div>
 </template>
 <script>
-  export default {}
+    import data from '../data'
+  export default {
+    methods:{
+      displayDetails (id) {
+        this.$router.push({name: 'detail', params:{id:id}})
+      }
+    },
+    data() {
+      return {
+        'picture': data.pictures
+      }
+    }
+  }
 </script>
 <style scoped>
 
--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,7 +3,19 @@
   </ul>
 </template>
 <script>
-  export default {}
+  import data from '../data'
+  export default {
+    methods:{
+      displayDetails (id) {
+        this.$router.push({name: 'detail', params:{id:id}})
+      }
+    },
+    data() {
+      return {
+        'picture': data.pictures
+      }
+    }
+  }
 </script>
 <style scoped>
   .list {
